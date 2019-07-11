004cd8349445bcda5cface57f540cdd461716c91
Hugo Aparicio <hugo.aparicio8@gmail.com>
2019-07-11 14:32:27 -0700

    Create Services.js

---
+++ src/components/Services.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Services extends Component {
+    render() {
+      return (
+      <div classname='ServiceDiv'>
+        <h1>Services</h1>
+        <p>We offer a wide range of services to make your haircut complete</p>
+      </div> 
+    )} }
+  
+  export default Services;
+  
