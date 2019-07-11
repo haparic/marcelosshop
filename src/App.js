cab19d1b52c9e3aca268b50acd084f7f39580408
Hugo Aparicio <hugo.aparicio8@gmail.com>
2019-07-11 14:32:41 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import Pics from './components/Pics';
+import About from './components/About';
+import Landing from './components/Landing';
+import Services from './components/Services';
+import { Route, Link } from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <header className="Header">
+        <h1>Marcelos Barber Shop</h1>
+        <div>
+        <nav>
+             <Link to='/'>Home</Link>
+             <Link to='/about'>About Us</Link>
+             <Link to='/services'>Services</Link>
+        </nav>
+        </div>
       </header>
-    </div>
-  );
-}
+      <main>
+        <Route exact path="/" component={Landing} />
+        <Route path="/about" component={About} />
+        <Route path="/services" component={Services} />
+      </main>
+      <div>
+        <Pics />
+      </div>
+    </div> 
+  )} }
 
 export default App;
+
+
