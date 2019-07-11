7d59dd665abf96c73b887e0e08c5cbc9e3ab1339
Hugo Aparicio <hugo.aparicio8@gmail.com>
2019-07-11 14:32:30 -0700

    Create Pics.js

---
+++ src/components/Pics.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class Pics extends Component {
+    render() {
+      return (
+      <div classname='Pics'>
+        <h1>Pics</h1>
+      </div> 
+    )} }
+  
+  export default Pics;
+  
