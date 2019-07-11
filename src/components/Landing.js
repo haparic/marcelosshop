76d1a60662af4e91aa6ff9a8cadb2636bececd34
Hugo Aparicio <hugo.aparicio8@gmail.com>
2019-07-11 14:32:34 -0700

    Create Landing.js

---
+++ src/components/Landing.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Landing extends Component {
+    render() {
+      return (
+      <div classname='Landing'>
+        <h1>Welcome to Marcelo's!</h1>
+        <p>Offering the freshest cuts in the Valley of The Sun</p>
+      </div> 
+    )} }
+  
+  export default Landing;
+  
